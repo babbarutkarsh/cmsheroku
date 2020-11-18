771b1d56da92bacdbab94005c5cfb949dc64bc23
babbarutkarsh <46282290+babbarutkarsh@users.noreply.github.com>
2020-11-18 12:00:29 +0530

    second commit

--- CMS/settings.py
+++ CMS/settings.py
@@ -1,4 +1,4 @@
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'abcdefghi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,3 +117,13 @@ STATIC_URL = '/static/'
 import os
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+django_heroku.settings(locals())
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
